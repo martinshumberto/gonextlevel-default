537f2efbc5ccbc0a7d1d520a83acc69fdbe2c115
Wallace Fontes <lalafontes78@gmail.com>
2018-08-14 13:19:07 -0400

    ajustes

---
+++ resources/views/client/includes/chat.blade.php
@@ -0,0 +1,53 @@
+<div class="floated-chat-btn">
+  <i class="os-icon os-icon-mail-07"></i><span>Demo Chat</span>
+</div>
+<div class="floated-chat-w">
+  <div class="floated-chat-i">
+    <div class="chat-close">
+      <i class="os-icon os-icon-close"></i>
+    </div>
+    <div class="chat-head">
+      <div class="user-w with-status status-green">
+        <div class="user-avatar-w">
+          <div class="user-avatar">
+            <img alt="" src="img/avatar1.jpg">
+          </div>
+        </div>
+        <div class="user-name">
+          <h6 class="user-title">
+            John Mayers
+          </h6>
+          <div class="user-role">
+            Account Manager
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="chat-messages">
+      <div class="message">
+        <div class="message-content">
+          Hi, how can I help you?
+        </div>
+      </div>
+      <div class="date-break">
+        Mon 10:20am
+      </div>
+      <div class="message">
+        <div class="message-content">
+          Hi, my name is Mike, I will be happy to assist you
+        </div>
+      </div>
+      <div class="message self">
+        <div class="message-content">
+          Hi, I tried ordering this product and it keeps showing me error code.
+        </div>
+      </div>
+    </div>
+    <div class="chat-controls">
+      <input class="message-input" placeholder="Type your message here..." type="text">
+      <div class="chat-extra">
+        <a href="#"><span class="extra-tooltip">Attach Document</span><i class="os-icon os-icon-documents-07"></i></a><a href="#"><span class="extra-tooltip">Insert Photo</span><i class="os-icon os-icon-others-29"></i></a><a href="#"><span class="extra-tooltip">Upload Video</span><i class="os-icon os-icon-ui-51"></i></a>
+      </div>
+    </div>
+  </div>
+</div>
