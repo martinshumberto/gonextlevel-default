a9e6e4a2cfbb46c48442a13ce770d27db880c0cb
wallace <lalafontes78@gmail.com>
2018-08-14 00:21:27 -0400

    gerenate tbles

--- database/migrations/2018_08_14_012109_create_tb_discounts_clients_table.php
+++ database/migrations/2018_08_14_012109_create_tb_discounts_clients_table.php
@@ -19,7 +19,8 @@ class CreateTbDiscountsClientsTable extends Migration {
 			$table->integer('client_id');
 			$table->integer('plan_id');
 			$table->string('discount_code')->nullable();
-			$table->decimal('discount_price', 10)->nullable();
+			$table->integer('discount_type');
+			$table->integer('discount_price')->nullable();
 			$table->integer('status');
 			$table->timestamps();
 		});
